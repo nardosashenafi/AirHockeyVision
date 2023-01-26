6f39e539a4510b38b7c548b4108bc5b3c74bcd09
Daniel <danielpullicar@u.boisestate.edu>
2023-01-26 10:22:24 -0700

    Signed-off-by: Daniel <danielpullicar@u.boisestate.edu>
    Updated Live Calibrate to only export necassary arrays. Much more efficient
    DetectWithConverttoWorld imports the arrays, but does not yet convert to world coordinates. WIP.

--- pythonProject1/DetectWithConverttoWorld.py
+++ pythonProject1/DetectWithConverttoWorld.py
@@ -21,18 +21,30 @@ import glob
 
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-chessh=7;
-chessw=7;
-cameraName = 'CameraName'
+chessh = 7
+chessw = 7
+calibrationArrays = np.load("CameraArraysCameraName.npz", allow_pickle=True)
+print(calibrationArrays.files)
+camMtx = calibrationArrays['arr_0']
+distMtx = calibrationArrays['arr_1']
+rotVec = calibrationArrays['arr_2']
+transVec = calibrationArrays['arr_3']
+print(camMtx)
+print(distMtx)
+print(rotVec)
+print(transVec)
+
 objp = np.zeros((chessh * chessw, 3), np.float32)
 objp[0:, :2] = np.mgrid[0:7, 0:7].T.reshape(-1, 2)
-frameNum=0;
+frameNum = 0;
 
 # Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
 imgpoints = []  # 2d points in image plane.
 frames = []  # Frames take from camera
 cam = cv.VideoCapture(0)
+
+am = cv.VideoCapture(0)
 
 if not cam.isOpened():
     print("Cannot open camera")
@@ -46,65 +58,34 @@ while True:
     if not ret:
         print("failed to grab frame")
         break
-    # Turn image gray
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    undistortedFrame = cv.undistort(frame, camMtx, distMtx, None)
+    gray = cv.cvtColor(undistortedFrame, cv.COLOR_BGR2GRAY)
 
     # Return chessboard corner locations
     ret, corners = cv.findChessboardCorners(gray, (chessh, chessw), None)
+
     if ret:  # if corners are returned
         # Create corner objects at corner locations
         corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-
         # Draw and display the corners to original frame
-        frame = cv.drawChessboardCorners(frame, (chessh, chessw), corners2, ret)
+        frame = cv.drawChessboardCorners(undistortedFrame, (chessh, chessw), corners2, ret)
 
-    # Display the resulting frame
-    cv.imshow("Webcam", frame)
+    cv.imshow("Webcam", undistortedFrame)
+
     k = cv.waitKey(1)
-    if k%256 == 32:  # If space is pressed,
+    if k % 256 == 32:  # If space is pressed,
         if ret:  # If corners have been returned
-            # Add data from current frame to arrays
-            objpoints.append(objp)
-            imgpoints.append(corners2)
+            #Use Recover Pose for this part
+            imgmtx = [[corners2[0][0][0]],[corners2[0][0][1]]]
+            print(imgmtx)
 
-            frameNum = frameNum+1
+
+            #print(objpoints)
         else:  # If no corners were returned
             print("Chessboard not detected in image")
-    if k%256 == 27:  # If ESC is pressed
-
-        print("Frame collection complete..")
-        break
-
-# Use chessboard corners to get camera matrix, distortion coefficients, rotation and translation vectors etc.
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-
-# prepare image for undistortion
-ret, frame = cam.read()
-h,  w = frame.shape[:2]
-newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-
-while True:
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
-        break
-    if ret:
-        # undistort
-        dst = cv.undistort(frame, mtx, dist, None, newcameramtx)
-        # crop the image
-        x, y, w, h = roi
-        dst = dst[y:y + h, x:x + w]
-    cv.imshow("Webcam", dst)
-
-    k = cv.waitKey(1)
     if k%256 == 27:
         # ESC pressed
         print("Escape hit, closing...")
-        np.savez("CameraArrays" +cameraName,newcameramtx,dist,rvecs[-1],tvecs[-1])
-        print(newcameramtx)
-        print(dist)
-        print(rvecs[-1])
-        print(tvecs[-1])
         break
 cam.release()
 cv.destroyAllWindows()
