304b78e5b9a48cb9e8b6cc9582b99b63cdf004a0
roscoeambrose <roscoeambrose@u.boisestate.edu>
2023-04-06 15:30:29 -0600

    Missing parameters added to Caller.

--- MyCode/Caller.py
+++ MyCode/Caller.py
@@ -7,10 +7,10 @@ Created on Tue Feb 28 10:33:44 2023
 from HoughCirclesClass import *
 from threading import Thread
 
-#cameraNumber,width,height,tog_autoF,tog_autoE,exposure,focus,contrast,brightness,fps
+#camera,cameraNumber,width,height,tog_autoF,tog_autoE,exposure,focus,contrast,brightness,fps,blur,dp,minDist,minRadius,maxRadius,circleSensitivity,circleEdgePoints,saturation,hue,gain
 camera = CircleDetectionTestModeWindows(0,1280,720,0,0,-3,0.5,75,175,30,17,1.2,10000,5,50,100,40,125,0,10)    
                                         # TODO: extract these variables from a stored file
                                         # TODO: these variables will be saved to a file from the GUI
 
 Thread(target = camera.createGUI).start()
-#Thread(target = camera.detectionProgram(1)).start()
+Thread(target = camera.detectionProgram(1)).start()
