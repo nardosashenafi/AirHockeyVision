4dbfb1f9e2ad000fcd4fc478bd22dd2161e8506e
roscoeambrose <roscoeambrose@u.boisestate.edu>
2023-04-13 19:15:36 -0600

    All variables passed in to camera initializer call

--- MyCode/Caller.py
+++ MyCode/Caller.py
@@ -8,9 +8,9 @@ from HoughCirclesClass import *
 from threading import Thread
 
 #camera,cameraNumber,width,height,tog_autoF,tog_autoE,exposure,focus,contrast,brightness,fps,blur,dp,minDist,minRadius,maxRadius,circleSensitivity,circleEdgePoints,saturation,hue,gain
-camera = CircleDetectionTestModeWindows(0,1280,720,0,0,-3,0.5,75,175,30,17,1.2,10000,5,50,100,40,125,0,10)    
-                                        # TODO: extract these variables from a stored file
-                                        # TODO: these variables will be saved to a file from the GUI
+camera = CircleDetectionTestModeWindows(0,1280,720,0,0,-3,0.5,75,175,30,17,1.2,10000,5,50,100,40,125,0,10)
 
+mode = 1 # 0 - run mode, 1 - test mode
 Thread(target = camera.createGUI).start()
-Thread(target = camera.detectionProgram(1)).start()
+Thread(target = camera.detectionProgram(mode)).start()
+    
