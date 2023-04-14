62226751eb2a0e09e4c7939b7ca912733870efa3
roscoeambrose <roscoeambrose@u.boisestate.edu>
2023-04-13 19:28:32 -0600

    Fixed FPS paramater to 60

--- MyCode/Caller.py
+++ MyCode/Caller.py
@@ -8,7 +8,7 @@ from HoughCirclesClass import *
 from threading import Thread
 
 #camera,cameraNumber,width,height,tog_autoF,tog_autoE,exposure,focus,contrast,brightness,fps,blur,dp,minDist,minRadius,maxRadius,circleSensitivity,circleEdgePoints,saturation,hue,gain
-camera = CircleDetectionTestModeWindows(0,1280,720,0,0,-3,0.5,75,175,30,17,1.2,10000,5,50,100,40,125,0,10)
+camera = CircleDetectionTestModeWindows(0,1280,720,0,0,-3,0.5,75,175,60,17,1.2,10000,5,50,100,40,125,0,10)
 
 mode = 1 # 0 - run mode, 1 - test mode
 Thread(target = camera.createGUI).start()
