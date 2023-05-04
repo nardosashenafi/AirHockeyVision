574358346f49b63a771758c72f8720abb476b729
roscoeambrose <roscoeambrose@u.boisestate.edu>
2023-05-04 14:33:48 -0600

    Added cameraPort variable to simplify changes

--- Caller.py
+++ Caller.py
@@ -2,8 +2,9 @@ from HoughCirclesClass import *
 from threading import Thread
 
 def main():
+	cameraPort = 0
 	#camera,blur,dp,minDist,minRadius,maxRadius,circleSensitivity,circleEdgePoints,brightness,contrast,saturation,hue,gain,exposure,tog_autoE,focus,tog_autoF,cameraNumber,width,height,fps
-	camera = CircleDetectionTestModeWindows(17,1.2,10000,5,50,100,40,175,75,125,0,10,-7,0,255,1,0,1280,720,60)
+	camera = CircleDetectionTestModeWindows(17,1.2,10000,5,50,100,40,175,75,125,0,10,-7,0,255,1,cameraPort,1280,720,60)
 	Thread(target = camera.createGUI).start()
 	Thread(target = camera.detectionProgram(1)).start()
 
