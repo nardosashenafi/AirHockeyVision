a16f251f89fd84021f34ad7a6e179ed39b55ae4a
roscoeambrose <roscoeambrose@u.boisestate.edu>
2023-05-04 11:28:20 -0600

    Added AltCaller for starting the GUI standalone

--- AltCaller.py
+++ AltCaller.py
@@ -5,7 +5,6 @@ def startup():
  	#camera,blur,dp,minDist,minRadius,maxRadius,circleSensitivity,circleEdgePoints,brightness,contrast,saturation,hue,gain,exposure,tog_autoE,focus,tog_autoF,cameraNumber,width,height,fps
 	camera = CircleDetectionTestModeWindows(17,1.2,10000,5,50,100,40,175,75,125,0,10,-7,0,255,1,0,1280,720,60)
 	Thread(target = camera.createGUI).start()
-	Thread(target = camera.detectionProgram(1)).start()
 
 def main():
 	startup()
