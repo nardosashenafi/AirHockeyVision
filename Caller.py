37bc8a402945df0d3fad0ab3af51e660c56e5030
roscoeambrose <roscoeambrose@u.boisestate.edu>
2023-05-04 13:56:42 -0600

    Removed redundant function

--- Caller.py
+++ Caller.py
@@ -1,14 +1,11 @@
 from HoughCirclesClass import *
 from threading import Thread
 
-def startup():
- 	#camera,blur,dp,minDist,minRadius,maxRadius,circleSensitivity,circleEdgePoints,brightness,contrast,saturation,hue,gain,exposure,tog_autoE,focus,tog_autoF,cameraNumber,width,height,fps
+def main():
+	#camera,blur,dp,minDist,minRadius,maxRadius,circleSensitivity,circleEdgePoints,brightness,contrast,saturation,hue,gain,exposure,tog_autoE,focus,tog_autoF,cameraNumber,width,height,fps
 	camera = CircleDetectionTestModeWindows(17,1.2,10000,5,50,100,40,175,75,125,0,10,-7,0,255,1,0,1280,720,60)
 	Thread(target = camera.createGUI).start()
 	Thread(target = camera.detectionProgram(1)).start()
-
-def main():
-	startup()
 
 if __name__ == "__main__":
 	main()
