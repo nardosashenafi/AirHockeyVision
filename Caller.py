2602723764b7caf29e683e2a0450fa71ccc4a7ea
roscoeambrose <roscoeambrose@u.boisestate.edu>
2023-05-04 14:39:43 -0600

    Added testMode variable for simplicity of changing

--- Caller.py
+++ Caller.py
@@ -3,10 +3,11 @@ from threading import Thread
 
 def main():
 	cameraPort = 0
+	testMode = 1 # 0 = run mode, 1 = test mode
 	#camera,blur,dp,minDist,minRadius,maxRadius,circleSensitivity,circleEdgePoints,brightness,contrast,saturation,hue,gain,exposure,tog_autoE,focus,tog_autoF,cameraNumber,width,height,fps
 	camera = CircleDetectionTestModeWindows(17,1.2,10000,5,50,100,40,175,75,125,0,10,-7,0,255,1,cameraPort,1280,720,60)
 	Thread(target = camera.createGUI).start()
-	Thread(target = camera.detectionProgram(1)).start()
+	Thread(target = camera.detectionProgram(testMode)).start()
 
 if __name__ == "__main__":
 	main()
